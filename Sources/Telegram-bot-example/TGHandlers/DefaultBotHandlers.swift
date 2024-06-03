f9bcf5d5374477eff59026271a9319664abb1b26
starsden <159563356+starsden@users.noreply.github.com>
2024-06-03 21:53:25 +0300

    done

--- Sources/Telegram-bot-example/TGHandlers/DefaultBotHandlers.swift
+++ Sources/Telegram-bot-example/TGHandlers/DefaultBotHandlers.swift
@@ -4,6 +4,7 @@ import TelegramVaporBot
 final class DefaultBotHandlers {
     private static var userStates: [Int64: BookingState] = [:]
     private static let adminUserId: Int64 = 6627628549
+    private static var bookingA: [Int64: Int64] = [:]
 
     static func addHandlers(app: Vapor.Application, connection: TGConnectionPrtcl) async {
         await commandstatkHandler(app: app, connection: connection)
@@ -19,12 +20,12 @@ final class DefaultBotHandlers {
             ]
             let keyboard: TGInlineKeyboardMarkup = .init(inlineKeyboard: buttons)
             let params: TGSendMessageParams = .init(chatId: .chat(userId),
-                                                    text: "Доброго времени суток! \nЯ помогу вам забронировать время для прогулки с вашей собакой.",
+                                                    text: "Доброго времени суток!👋 \nЯ помогу вам забронировать время для прогулки с вашей собакой🐶",
                                                     replyMarkup: .inlineKeyboardMarkup(keyboard))
             try await bot.sendMessage(params: params)
         })
     }
-            ///Имя хозяина начало
+
     private static func bookHandler(app: Vapor.Application, connection: TGConnectionPrtcl) async {
         await connection.dispatcher.add(TGCallbackQueryHandler(pattern: "time") { update, bot in
             guard let userId = update.callbackQuery?.from.id else { return }
@@ -32,6 +33,7 @@ final class DefaultBotHandlers {
             let params: TGSendMessageParams = .init(chatId: .chat(userId), text: "Как вас зовут?")
             try await bot.sendMessage(params: params)
         })
+
         await connection.dispatcher.add(TGMessageHandler(filters: .all) { update, bot in
             guard let userId = update.message?.from?.id else { return }
             guard let state = userStates[userId] else { return }
@@ -39,48 +41,28 @@ final class DefaultBotHandlers {
             switch state {
             case .nameus:
                 userStates[userId] = .dogname(userName: update.message?.text ?? "")
-            ///имя хозяина конец
-                
-            ///имя собаки начало
                 let params: TGSendMessageParams = .init(chatId: .chat(userId), text: "Как зовут вашу собаку?")
                 try await bot.sendMessage(params: params)
             case .dogname(let userName):
                 userStates[userId] = .dogclass(userName: userName, dogName: update.message?.text ?? "")
-           ///имя собаки конец
-                
-            ///порода начало
                 let params: TGSendMessageParams = .init(chatId: .chat(userId), text: "Какой породы собака?")
                 try await bot.sendMessage(params: params)
             case .dogclass(let userName, let dogName):
                 userStates[userId] = .time(userName: userName, dogName: dogName, dogClass: update.message?.text ?? "")
-            ///порода конец
-                
-            ///когда забрать нчало
                 let params: TGSendMessageParams = .init(chatId: .chat(userId), text: "Когда нужно забрать \(dogName)?")
                 try await bot.sendMessage(params: params)
             case .time(let userName, let dogName, let dogClass):
                 userStates[userId] = .loca(userName: userName, dogName: dogName, dogClass: dogClass, time: update.message?.text ?? "")
-            ///когда забрать конец
-                
-            
-            ///откуда забрать начало
                 let params: TGSendMessageParams = .init(chatId: .chat(userId), text: "Откуда забрать \(dogName)?")
                 try await bot.sendMessage(params: params)
             case .loca(let userName, let dogName, let dogClass, let time):
                 userStates[userId] = .phnum(userName: userName, dogName: dogName, dogClass: dogClass, time: time, loca: update.message?.text ?? "")
-            ///Откуда забрать конец
-                
-            
-                
-                
-            ///номер телефона начало
                 let buttons: [[TGKeyboardButton]] = [
                     [.init(text: "Отправить свой номер", requestContact: true)]
                 ]
                 let keyboard: TGReplyKeyboardMarkup = .init(keyboard: buttons, oneTimeKeyboard: true)
                 let params: TGSendMessageParams = .init(chatId: .chat(userId), text: "Как с вами связаться?", replyMarkup: .replyKeyboardMarkup(keyboard))
                 try await bot.sendMessage(params: params)
-
             case .phnum(let userName, let dogName, let dogClass, let time, let loca):
                 let phnum: String
                 if let contact = update.message?.contact {
@@ -88,17 +70,15 @@ final class DefaultBotHandlers {
                 } else {
                     phnum = update.message?.text ?? ""
                 }
-            ///номер телефона конец
-                
-                
                 let booking = Booking(userName: userName, dogName: dogName, dogClass: dogClass, time: time, phnum: phnum, loca: loca)
+                bookingA[userId] = adminUserId
                 try await sendBookingToAdmin(booking: booking, bot: bot)
                 userStates.removeValue(forKey: userId)
-                let params: TGSendMessageParams = .init(chatId: .chat(userId), text: "Бронирование успешно завершено. Спасибо!")
+                let params: TGSendMessageParams = .init(chatId: .chat(userId), text: "Бронирование успешно завершено❤️ \nСпасибо!")
                 try await bot.sendMessage(params: params)
             }
         })
-//УЗНАТЬ ПОДРОБНЕЕ НАЧАЛО
+
         await connection.dispatcher.add(TGCallbackQueryHandler(pattern: "moreinf") { update, bot in
             guard let userId = update.callbackQuery?.from.id, let messageId = update.callbackQuery?.message?.messageId else { return }
             try await bot.deleteMessage(params: .init(chatId: .chat(userId), messageId: messageId))
@@ -106,7 +86,7 @@ final class DefaultBotHandlers {
                 [.init(text: "Вернуться", callbackData: "goback")]
             ]
             let keyboard: TGInlineKeyboardMarkup = .init(inlineKeyboard: buttons)
-            let params: TGSendMessageParams = .init(chatId: .chat(userId), text: "Гуляю всех собак, от больших до маленьких. Люблю всех. \n\n400 рублей  - 40 минут", replyMarkup: .inlineKeyboardMarkup(keyboard))
+            let params: TGSendMessageParams = .init(chatId: .chat(userId), text: "Гуляю всех собак, от больших до маленьких. Люблю всех🐕 \n\n400 рублей  - 40 минут", replyMarkup: .inlineKeyboardMarkup(keyboard))
             try await bot.sendMessage(params: params)
         })
 
@@ -118,13 +98,22 @@ final class DefaultBotHandlers {
             ]
             let keyboard: TGInlineKeyboardMarkup = .init(inlineKeyboard: buttons)
             let params: TGSendMessageParams = .init(chatId: .chat(userId),
-                                                    text: "Доброго времени суток! \nЯ помогу вам забронировать время для прогулки с вашей собакой.",
+                                                    text: "Доброго времени суток!👋 \nЯ помогу вам забронировать время для прогулки с вашей собакой🐶",
                                                     replyMarkup: .inlineKeyboardMarkup(keyboard))
             try await bot.sendMessage(params: params)
-//УЗНАТЬ ПОДРОБНЕЕ КОНЕЦ
+        })
+
+        await connection.dispatcher.add(TGCallbackQueryHandler(pattern: "сancelbook") { update, bot in
+            guard let adminId = update.callbackQuery?.from.id, adminId == adminUserId else { return }
+            guard let userId = bookingA.first(where: { $0.value == adminUserId })?.key else { return }
+            
+            let userParams: TGSendMessageParams = .init(chatId: .chat(userId), text: "Ваше бронирование было отменено по решению администратора😢 \n\nВы можете связаться с ним для выяснения обстоятельств: @hahaaka ")
+            try await bot.sendMessage(params: userParams)
+            
+            bookingA.removeValue(forKey: userId)
         })
     }
-///шаблон отправки админу
+
     private static func sendBookingToAdmin(booking: Booking, bot: TGBotPrtcl) async throws {
         let message = """
         Новое бронирование✅:
@@ -135,7 +124,11 @@ final class DefaultBotHandlers {
         Адрес: \(booking.loca)
         Контакты: \(booking.phnum)
         """
-        let params: TGSendMessageParams = .init(chatId: .chat(adminUserId), text: message)
+        let buttons: [[TGInlineKeyboardButton]] = [
+            [.init(text: "❌Отказаться❌", callbackData: "сancelbook")]
+        ]
+        let keyboard: TGInlineKeyboardMarkup = .init(inlineKeyboard: buttons)
+        let params: TGSendMessageParams = .init(chatId: .chat(adminUserId), text: message, replyMarkup: .inlineKeyboardMarkup(keyboard))
         try await bot.sendMessage(params: params)
         print(message)
     }
